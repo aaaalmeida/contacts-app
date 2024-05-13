1265b793ff852f32ed46a2eb0d98b4e8149c9cf8
aaaalmeida <arthurfonsecaalmeida@gmail.com>
2024-05-13 12:20:36 -0300

    finished configurating typescript and tailwinds

--- src/components/ContactCard/index.tsx
+++ src/components/ContactCard/index.tsx
@@ -1,8 +1,8 @@
 import React from "react"
 export const ContactCard = () => {
   return (
-    <div>
-      ContactCard123
+    <div className="text-9xl bg-red-200">
+      ContactCard
     </div>
   )
 }
