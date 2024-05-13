7de09aa0e997da7dc59c4187736d73b6767bbcdc
aaaalmeida <arthurfonsecaalmeida@gmail.com>
2024-05-13 01:41:43 -0300

    initializing contacts project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ContactCard } from './components/ContactCard';
+
+function App() {
+  return (
+    <div className="App">
+      <ContactCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ContactCard/index.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+export const ContactCard = () => {
+  return (
+    <div>
+      ContactCard123
+    </div>
+  )
+}
